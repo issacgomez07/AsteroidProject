bfda112fe405b5220cda4e743b9fa79d2585e8a4
Issac Gomez <issac.gomez0207@gmail.com>
2025-05-20 01:26:46 +0000

    Added player class

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable) # type: ignore
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius = PLAYER_RADIUS)
     
     while True:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+                
         screen.fill(color = "black")
-
+        for d in drawable:
+            d.draw(screen)
+        for u in updatable:
+            u.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
